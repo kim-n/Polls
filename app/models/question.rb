14f3ef1801a419706a2e171b87dd9c909872cf38
App Academy Student <student@appacademy.io>
2013-11-07 16:11:53 -0500

    Adds poll results

--- app/models/question.rb
+++ app/models/question.rb
@@ -17,4 +17,14 @@ class Question < ActiveRecord::Base
     :foreign_key => :question_id,
     :primary_key => :id
   )
+
+  def results
+    answer_choices_with_counts = self.answer_choices.select("answer_choices.*, COUNT(responses.id) AS response_count").joins("LEFT OUTER JOIN responses ON responses.answer_id = answer_choices.id").group("answer_choices.id")
+
+    answer_counts = {}
+    answer_choices_with_counts.map! do |answer_choice|
+      answer_counts[answer_choice.choice] = answer_choice.response_count
+    end
+    answer_counts
+  end
 end
